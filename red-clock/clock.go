bcc064adddece8a30a4cd0c2b77d0047dd8989f7
miladsrc <105477498+miladsrc@users.noreply.github.com>
2024-10-20 09:30:03 +0330

    feat: clock array string

---
+++ red-clock/clock.go
@@ -0,0 +1,96 @@
+package main
+
+func main() {
+
+	type placeholder [5]strign
+
+	zero := placeholder{
+		" ███ ",
+		"█   █",
+		"█   █",
+		"█   █",
+		" ███ ",
+	}
+
+	one := placeholder
+	{
+			" ██  ",
+			"  █  ",
+			"  █  ",
+			"  █  ",
+			" ███ ",
+	}
+
+	two := placeholder
+	{
+			" ███ ",
+			"    █",
+			" ███ ",
+			"█    ",
+			"█████",
+	}
+
+	three := placeholder
+	{
+			" ███ ",
+			"    █",
+			" ███ ",
+			"    █",
+			" ███ ",
+	}
+
+	four := placeholder
+	{
+			"█   █",
+			"█   █",
+			"█████",
+			"    █",
+			"    █",
+	}
+
+	five := placeholder
+	{
+			"█████",
+			"█    ",
+			"████ ",
+			"    █",
+			" ███ ",
+	}
+
+	six := placeholder
+	{
+			" ███ ",
+			"█    ",
+			"████ ",
+			"█   █",
+			" ███ ",
+	}
+
+	seven := placeholder
+	{
+			"█████",
+			"    █",
+			"   █ ",
+			"  █  ",
+			" █   ",
+	}
+
+	eight := placeholder
+	{
+			" ███ ",
+			"█   █",
+			" ███ ",
+			"█   █",
+			" ███ ",
+	}
+
+	nine := placeholder
+	{
+			" ███ ",
+			"█   █",
+			" ████",
+			"    █",
+			" ███ ",
+	}
+
+}
